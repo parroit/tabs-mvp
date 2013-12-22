b17c30991da1e11302369fe749463366cde0ade3
parroit <andrea.parodi@ebansoftware.net>
2013-12-22 10:18:49 +0100

    improved readme

--- test/tabs_test.js
+++ test/tabs_test.js
@@ -1,24 +1,51 @@
 var assert = require("assert"),
     expect = require("expect.js"),
     _ = require("lodash"),
-    Tabs = require("../lib/tabs"),
+    makeTabs = require("../lib/tabs"),
 
-    tabs = new Tabs();
+    tabs = makeTabs();
 
+function makeTab(caption){
+    return {
+        active:false,
+        caption: caption
+    };
+}
 
 describe("Tabs", function () {
+    var tab1 = makeTab("test1"),
+        tab2 = makeTab("test2");
+
     it("is defined", function () {
-        expect(Tabs).to.be.an('function');
+        expect(makeTabs).to.be.an('function');
     });
 
-    it("is is instantiable", function () {
-        expect(tabs).to.be.an('object');
+    it("is instantiable", function () {
+        expect(tabs).to.be.an('array');
     });
 
-    it("is emits events", function () {
+    it("emits events", function () {
         expect(tabs.events).to.be.an('object');
     });
 
+    it("activate first added tab", function () {
+
+        tabs.push(tab1);
+        expect(tab1.active).to.be.equal(true);
+    });
+
+    it("doesn't activate second added tab", function () {
+
+        tabs.push(tab2);
+        expect(tab1.active).to.be.equal(true);
+        expect(tab2.active).to.be.equal(false);
+    });
+
+    it("activate last tab remaining", function () {
+
+        tabs.remove(0);
+        expect(tab2.active).to.be.equal(true);
+    });
 
 
 });
